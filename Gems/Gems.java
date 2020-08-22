f078d17e62c12e329990a5d2ab6ffa5ec7f404b4
Darrious <darriousabarger@gmail.com>
2020-08-21 23:21:29 -0400

    Finished assignment

--- Gems/Gems.java
+++ Gems/Gems.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Gems
 {
+	//This method generates random gem values between 0 and 50000
 	public int[] generateVals(int total)
 	{
 		Random r = new Random();
@@ -16,6 +17,7 @@ public class Gems
 			gemsVal[i] = r.nextInt(50000);
 		}
 
+		System.out.println("Gem Values:");
 		for(int i = 0; i < gemsVal.length; i++)
 		{
 			System.out.print(gemsVal[i] + " ");
@@ -25,8 +27,99 @@ public class Gems
 
 	}
 	
+	// I implemented a bubble sort algorithm to sort the gems
+	public int[] sortGems(int[] gems)
+	{
+		boolean sorted = false;
+		int temp = 0;
 
-	
+		while(true)
+		{
+			sorted = true;
+			for (int i = 0; i < gems.length; i++)
+			{
+				if (i < gems.length - 1)
+				{
+					//System.out.println("i = " + gems[i] + " i+1 = " gems[i+1]);
+
+					if (gems[i+1] < gems[i])
+					{
+						sorted = false;
+						temp = gems[i+1];
+						gems[i+1] = gems[i];
+						gems[i] = temp;
+					}
+				}
+			}
+			if(sorted == true)
+			{
+				break; // I know this is frowned upon
+			}
+
+			
+
+		}
+
+		/* Prints sorted gems
+		System.out.print("\n\n");
+		for (int i = 0; i < gems.length; i++)
+		{
+			System.out.print(gems[i] + " ");
+		}
+		*/
+
+
+		return gems;
+	}
+
+	// This method starts at the smallest value of the list and takes turns adding to each pile
+	public int[][] divide(int[] gems)
+	{
+		int[] pile1 = new int[gems.length];
+		int[] pile2 = new int[gems.length];
+		int sum1 = 0, sum2 = 0;
+
+		for (int i = 0; i < gems.length; i+=2)
+		{
+			pile1[i] = gems[i];
+
+			if ((i+1) < gems.length)
+			{
+					pile2[i+1] = gems[i+1];
+				
+			}
+
+			sum1 += gems[i];
+			if ((i+1) < gems.length)
+			{
+				sum2 += gems[i+1];
+			}
+		}
+
+
+		System.out.println("\n\nPile #1:");
+		for (int g : pile1)
+		{
+			if(g!=0)
+				System.out.print(g + " ");
+
+		}
+		System.out.println("\nSum = " + sum1);
+
+
+		System.out.println("\nPile #2:");
+		for (int g : pile2)
+		{	
+			if(g!=0)
+				System.out.print(g + " ");
+
+		}
+	    System.out.println("\nSum = " + sum2);
+
+		int[][] piles = {pile1, pile2};
+		return piles;
+
+	}
 	
 }
 
--- Gems/Lab1.java
+++ Gems/Lab1.java
@@ -1,23 +1,85 @@
 // Darrious Barger
 
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
 public class Lab1
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{   
 		Gems gem = new Gems();
 		Scanner input = new Scanner(System.in);
-		int total = 0;
+		int total = 0, choice = 0;
         
-        //Prompt user for number of gems
-		System.out.println("How many gems are in the bag?");
-		total = input.nextInt();
+		// Menu
+		while (choice != 1 && choice != 2)
+		{
+			System.out.println("Read from file - 1\nRandom Gems - 2");
+			choice = input.nextInt();
+		}
+        
 
-		//Create array the size of the number of gems
-		//This is used to hold the value of each gem
-		int gemsVal[] = gem.generateVals(total);
 
+		if(choice == 1)
+		{
+			// Prompts user for file path
+			System.out.println("Enter file path (./filename if the file is in this directory):" );
+			String fileName = input.nextLine();
+			ArrayList<Integer> gemList = new ArrayList<Integer>();
+			fileName = input.nextLine();
+			
+			// Creating File scanner
+			File file = new File(fileName);
+			Scanner fileReader = new Scanner(file);
+					
+			// Add gem values to arraylist
+			while(fileReader.hasNextLine())
+			{
+				gemList.add(fileReader.nextInt());
+			}
+
+			fileReader.close();
+			int[] gemsVal = new int[gemList.size()];
+
+
+			// Convert array list to array.. I think I'm making this complicated
+			int i = 0;
+		    System.out.println("\nGem Values: ");
+			for (int g : gemList)
+			{
+				System.out.print(g + " ");
+				gemsVal[i++] = g;
+			}
+
+
+			// This holds the gems after going through the bubble sort algorithm
+			int gemsSorted[] = gem.sortGems(gemsVal);
+
+			// This holds a two dimensial array of the piles
+			int gemsDivided[][] = gem.divide(gemsSorted);
+
+
+		}
+
+		if(choice == 2)
+		{
+	        //Prompt user for number of gems
+			System.out.println("\nHow many gems are in the bag?");
+			total = input.nextInt();
+
+			//Create array the size of the number of gems
+			//This is used to hold the value of each gem
+			int gemsVal[] = gem.generateVals(total);
+
+			// This holds the gems after going through the bubble sort algorithm
+			int gemsSorted[] = gem.sortGems(gemsVal);
+
+			// This holds a two dimensial array of the piles
+			int gemsDivided[][] = gem.divide(gemsSorted);
+		}
+	
 
 	}
 }
